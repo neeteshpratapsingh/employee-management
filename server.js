cc0c037af0623f7e553444ac0ee557eca490f286
Neetesh Pratap <npsparihar97@gmail.com>
2019-02-19 11:43:02 +0530

    Employer creation

--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const morgan = require('morgan');
-// const passport = require('passport');
-// const jsonwebtoken = require('jsonwebtoken');
-// const cors = require('cors');
-
-// const employers = require('./routes/api/employers');
-
-// mongoose.connect('mongodb://localhost:27017/employee', { useNewUrlParser: true });
-// const db = mongoose.connection;
-
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(jsonwebtoken);
-// app.use(morgan('dev'));
-// app.use(passport.initialize());
-
-// require('./config/passport')(passport);
-
-// app.use('/api/employers', employers);
-
-// const port = process.env.port || 6000;
-// app.listen(port, () => console.log(`server running on ${port}`));
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
